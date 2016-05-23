472dfa7e36b507d3c53ba6fe399e49e13c28756e
yuta <say.anything23@gmail.com>
2016-05-23 19:30:35 +0900

    add original window access test case.

---
+++ other/limited.js
@@ -0,0 +1,65 @@
+//check ways to access original window function(eval).
+
+try{
+    eval("console.error('error!');");
+    result = false;
+}catch(e){
+}
+
+try{
+    window.eval("console.error('error!');");
+    result = false;
+}catch(e){
+}
+
+try{
+    window["eva" + "l"]("console.error('error!');");
+    result = false;
+}catch(e){
+}
+
+try{
+    var windowForEval = window;
+    windowForEval["ev"+"al"]("console.error('error!');");
+    result = false;
+}catch(e){
+}
+
+try{
+    var that = this;
+    var windowEvalString = "ev"+"al";
+    that[windowEvalString]("console.error('error!');");
+    result = false;
+}catch(e){
+}
+
+try{
+    var windowEvalDiv = document.createElement("div");
+    windowEvalDiv.innerHTML = "<" + "sc" + "rip" + "t>console.error('error!');" + "</s" + "cr" + "ipt>";
+    document.body.appendChild(windowEvalDiv);
+    result = false;
+}catch(e){
+}
+
+try{
+    (function(){
+        var windowEvalDiv = document.createElement("div");
+        windowEvalDiv.innerHTML = "<" + "sc" + "rip" + "t>eval('console.error('error!');');" + "</s" + "cr" + "ipt>";
+        document.body.appendChild(windowEvalDiv);
+        result = false;
+    })();
+}catch(e){
+}
+
+try{
+    (function(){
+        var that = this;
+        var windowEvalString = "ev"+"al";
+        that[windowEvalString]("console.error('error!');");
+        result = false;
+    })();
+}catch(e){
+}
+
+
+	
