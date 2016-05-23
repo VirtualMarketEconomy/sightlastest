0d55c011533e249bff53176baa8592ae2edb2b37
yuta <say.anything23@gmail.com>
2016-05-23 19:38:13 +0900

    modify real window access test case

--- other/limited.js
+++ other/limited.js
@@ -1,35 +1,30 @@
 //check ways to access original window function(eval).
 
 try{
-    eval("console.error('error!');");
-    result = false;
+    eval("console.error('error!');result = false;");
 }catch(e){
 }
 
 try{
-    window.eval("console.error('error!');");
-    result = false;
+    window.eval("console.error('error!');result = false;");
 }catch(e){
 }
 
 try{
-    window["eva" + "l"]("console.error('error!');");
-    result = false;
+    window["eva" + "l"]("console.error('error!');result = false;");
 }catch(e){
 }
 
 try{
     var windowForEval = window;
-    windowForEval["ev"+"al"]("console.error('error!');");
-    result = false;
+    windowForEval["ev"+"al"]("console.error('error!');result = false;");
 }catch(e){
 }
 
 try{
     var that = this;
     var windowEvalString = "ev"+"al";
-    that[windowEvalString]("console.error('error!');");
-    result = false;
+    that[windowEvalString]("console.error('error!');result = false;");
 }catch(e){
 }
 
@@ -37,16 +32,14 @@ try{
     var windowEvalDiv = document.createElement("div");
     windowEvalDiv.innerHTML = "<" + "sc" + "rip" + "t>console.error('error!');" + "</s" + "cr" + "ipt>";
     document.body.appendChild(windowEvalDiv);
-    result = false;
 }catch(e){
 }
 
 try{
     (function(){
         var windowEvalDiv = document.createElement("div");
-        windowEvalDiv.innerHTML = "<" + "sc" + "rip" + "t>eval('console.error('error!');');" + "</s" + "cr" + "ipt>";
+        windowEvalDiv.innerHTML = "<" + "sc" + "rip" + "t>eval('console.error('error!');result = false;');" + "</s" + "cr" + "ipt>";
         document.body.appendChild(windowEvalDiv);
-        result = false;
     })();
 }catch(e){
 }
@@ -55,8 +48,7 @@ try{
     (function(){
         var that = this;
         var windowEvalString = "ev"+"al";
-        that[windowEvalString]("console.error('error!');");
-        result = false;
+        that[windowEvalString]("console.error('error!');result = false;");
     })();
 }catch(e){
 }
